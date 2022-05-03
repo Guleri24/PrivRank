a6c07f60488f30e97ba8c482f6a96a61510457f7
Guleri24 <guleri24@tuta.io>
2022-05-04 00:01:21 +0530

    Refactored and add Heroku initial setup

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def input_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
@@ -18,51 +19,50 @@ async def input_form(request: Request):
 
 @app.post("/", response_class=HTMLResponse)
 async def recommended_users(request: Request,
-                                liked_users: str = Form(...)):
+                            liked_users: str = Form(...)):
     recommended_users_arr = liked_users.split(',')
     recommended_users_arr = [int(e) for e in recommended_users_arr]
 
-    # add recommended users logic here
-    recommended_users_arr = obfuscation(recommended_users_arr, 10, 1000) # 1000 -> N; Also need to change in preprocess.py
+    # 1000 -> N; Also need to change in preprocess.py
+    recommended_users_arr = obfuscation(recommended_users_arr, 10, 1000)
     recommended_users_arr = predict(recommended_users_arr)
     res = ''
     for id, score in recommended_users_arr:
         res += f'Recommending item number {id} having prediction score {score} <br>'
     return res
-    #return '\n'.join(str(e) for e in recommended_users_arr)
 
-    # delim = ','
-    # recommended_users_str = delim.join(map(str, recommended_users_arr))
-    # return recommended_users_str
-    # return templates.TemplateResponse("form.html", {"request": request})
 
 def predict(arr):
     arr = set(arr)
     matrix = get_similarity_matrix()
     N = len(matrix)
 
-    scores = [[0,i] for i in range(0,N)]
+    scores = [[0, i] for i in range(0, N)]
     for liked_item in arr:
-        for idx in range(0,N):
+        for idx in range(0, N):
             scores[idx][0] += matrix[liked_item][idx]
     scores.sort(reverse=True)
     res = []
-    for score,val in scores:
-        if val not in arr: res += [[val,score]]
-        if len(res) == 10: break
+    for score, val in scores:
+        if val not in arr:
+            res += [[val, score]]
+        if len(res) == 10:
+            break
     return res
+
 
 def get_similarity_matrix():
     with open('similarity.csv', newline='') as file:
-        reader = csv.reader(file,delimiter=' ')
+        reader = csv.reader(file, delimiter=' ')
         matrix = []
         for row in reader:
-            matrix += [[float(e) for e in row]]   
+            matrix += [[float(e) for e in row]]
         return matrix
+
 
 def obfuscation(arr, THRESHOLD, N):
     THRESHOLD = int((THRESHOLD*N)/100)
-    THRESHOLD = min(THRESHOLD,N)
+    THRESHOLD = min(THRESHOLD, N)
     indexes = random.sample(range(0, N), THRESHOLD)
     res = set()
 
--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,25 @@
 import csv
-import random 
+import random
+
+
 def write_csv(matrix):
     with open('similarity.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=' ')
         for row in matrix:
             writer.writerow(row)
-            
+
+
 def f():
-    N = 1000 # 1000 -> N; Also need to change in main.py; obfuscation() call
-    res = [[1 for _ in range(0,N)] for _ in range(0,N)]
-    for i in range(0,N):
-        for j in range(i,N):
+    N = 1000  # 1000 -> N; Also need to change in main.py; obfuscation() call
+    res = [[1 for _ in range(0, N)] for _ in range(0, N)]
+    for i in range(0, N):
+        for j in range(i, N):
             if i != j:
                 x = random.random()
                 res[i][j] = x
                 res[j][i] = x
     return res
 
-result  = f()  
+
+result = f()
 write_csv(result)
