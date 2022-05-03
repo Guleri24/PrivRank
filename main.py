a8bfae2c5d4b36167c172e93c04e3c0e65c579d0
Guleri24 <guleri24@tuta.io>
2022-05-04 02:11:08 +0530

    Add UI for PrivRank Result

--- main.py
+++ main.py
@@ -26,23 +26,23 @@ async def recommended_users(request: Request,
     # 1000 -> N; Also need to change in preprocess.py
     recommended_users_arr = obfuscation(recommended_users_arr, 10, 1000)
     recommended_users_arr = predict(recommended_users_arr)
-    res = ''
+    res = []
     for id, score in recommended_users_arr:
-        res += f'Recommending item number {id} having prediction score {score} <br>'
-    return res
+        res.append(f"       {id}                               {score}")
+    return templates.TemplateResponse("result.html", {"request": request, "res": res})
 
 
 def predict(arr):
-    arr = set(arr)
-    matrix = get_similarity_matrix()
-    N = len(matrix)
+    arr=set(arr)
+    matrix=get_similarity_matrix()
+    N=len(matrix)
 
-    scores = [[0, i] for i in range(0, N)]
+    scores=[[0, i] for i in range(0, N)]
     for liked_item in arr:
         for idx in range(0, N):
             scores[idx][0] += matrix[liked_item][idx]
     scores.sort(reverse=True)
-    res = []
+    res=[]
     for score, val in scores:
         if val not in arr:
             res += [[val, score]]
@@ -53,18 +53,18 @@ def predict(arr):
 
 def get_similarity_matrix():
     with open('similarity.csv', newline='') as file:
-        reader = csv.reader(file, delimiter=' ')
-        matrix = []
+        reader=csv.reader(file, delimiter=' ')
+        matrix=[]
         for row in reader:
             matrix += [[float(e) for e in row]]
         return matrix
 
 
 def obfuscation(arr, THRESHOLD, N):
-    THRESHOLD = int((THRESHOLD*N)/100)
-    THRESHOLD = min(THRESHOLD, N)
-    indexes = random.sample(range(0, N), THRESHOLD)
-    res = set()
+    THRESHOLD=int((THRESHOLD*N)/100)
+    THRESHOLD=min(THRESHOLD, N)
+    indexes=random.sample(range(0, N), THRESHOLD)
+    res=set()
 
     for idx in set(indexes+arr):
         if random.randint(0, 1):
